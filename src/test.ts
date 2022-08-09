797e92a3c2afc19d64a55a27a3b9a3fcfd0b6caf
Saky <souhoc@gmail.com>
2022-08-09 21:39:42 +0200

    add: test script

--- src/test.ts
+++ src/test.ts
@@ -2,14 +2,9 @@ import "dotenv/config";
 import { Client } from "./index";
 
 (async () => {
-	const client = new Client(
-		<string>process.env.CLIENT_ID,
-		<string>process.env.CLIENT_SECRET
-	);
+	const client = new Client(<string>process.env.ID, <string>process.env.SECRET);
 
 	const user = await client.users.get("shocquen");
-	const events = await user?.events;
-	if (events) {
-		console.log(events[1]);
-	}
+
+	console.log(user?.displayname);
 })();
