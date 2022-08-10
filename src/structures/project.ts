4690ad5f3b124bf4eba351f89b0c653ecfe005b1
dhubleur <dhubleur@student.42.fr>
2022-08-10 11:56:21 +0200

    make projects structure more accurate to doc

--- src/structures/project.ts
+++ src/structures/project.ts
@@ -1,6 +1,22 @@
+import { ICampus } from "./campus";
+import { ICursus } from "./cursus";
+
 export interface IProject {
     id: number,
     name: string,
     slug: string,
-    parent_id: number
+    parent_id: number,
+    parent: IProject | null,
+    children: IProject[],
+    description: string,
+    objectives: string[],
+    tier: number,
+    attachments: Object[],
+    created_at: Date,
+    updated_at: Date,
+    exam: boolean,
+    cusus: ICursus,
+    campus: ICampus,
+    skills: Object[],
+    project_sessions: Object[]
 }
