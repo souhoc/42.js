59a6313bdfe025a03aa8ccd458b77139960e9c47
Saky <souhoc@gmail.com>
2022-08-10 12:31:21 +0200

    clean: test

--- src/test.ts
+++ src/test.ts
@@ -3,4 +3,10 @@ import { Client } from "./index";
 
 (async () => {
 	const client = new Client(<string>process.env.ID, <string>process.env.SECRET);
+
+	const name = encodeURIComponent("Crêpes BDE LLD");
+	const events = await client.events.fetch({
+		params: ["filter[name]=" + name],
+	});
+	console.log(events);
 })();
