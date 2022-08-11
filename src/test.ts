ff3f81eaf2673acae6085022932795e5dcae5912
Saky <souhoc@gmail.com>
2022-08-11 15:54:53 +0200

    update: package v

--- src/test.ts
+++ src/test.ts
@@ -4,9 +4,6 @@ import { Client } from "./index";
 (async () => {
 	const client = new Client(<string>process.env.ID, <string>process.env.SECRET);
 
-	const name = encodeURIComponent("Crêpes BDE LLD");
-	const events = await client.events.fetch({
-		params: ["filter[name]=" + name],
-	});
-	console.log(events);
+	const user = await client.users.get("shocquen");
+	console.log(user);
 })();
