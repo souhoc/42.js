6fe1e3ec63d8b172d57a3f9e3ee3f09498c8c42c
dhubleur <dhubleur@student.42.fr>
2022-10-09 15:22:42 +0200

    docs(TEST): Complete test.ts for atuh example

--- src/test.ts
+++ src/test.ts
@@ -4,7 +4,10 @@ import { Client } from "./index";
 (async () => {
 	const client = new Client(<string>process.env.ID, <string>process.env.SECRET);
 
-	const auth_process = await client.auth_manager.init_auth_process("http://localhost:3333/callback", ["public", "projects", "profile"], {port: 3333});
+	const auth_process = await client.auth_manager.init_auth_process("http://localhost:3333/callback", ["public", "projects", "profile"], {port: 3333, callback_function: (user) => {
+		if(auth_process)
+			client.auth_manager.stop_auth_process(auth_process.id);
+	}});
 	if(auth_process)
 		console.log(auth_process.url);
 })();
