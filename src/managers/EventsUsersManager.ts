6d354c6744f95bc696f8e1dba27d2db97dff4130
Saky <--replace-all>
2023-08-22 22:58:34 +0200

    fix: fetch now can go futher than the base limit... Gota catch them all!

--- src/managers/EventsUsersManager.ts
+++ src/managers/EventsUsersManager.ts
@@ -1,16 +1,20 @@
 import { BaseManager } from "./BaseManager";
 import { Client } from "../structures/client";
-import { EventsUsers } from "../structures/events_users";
+import { EventsUsers, IEventUser } from "../structures/events_users";
 
 export class EventsUsersManager extends BaseManager {
-	constructor(client: Client) {
-		super(client);
-	}
+  constructor(client: Client) {
+    super(client);
+  }
 
-	async fetch(event_id: number): Promise<EventsUsers[] | null> {
-		const res = await this.client.get("events/" + event_id + "/events_users");
-		const ret: EventsUsers[] = [];
-		(<EventsUsers[]>res?.data).forEach((e) => ret.push(new EventsUsers(e)));
-		return ret;
-	}
+  async fetch(
+    event_id: number,
+    options?: { limit?: number; params: string[] },
+  ) {
+    const res = await this.client.fetch(
+      "events/" + event_id + "/events_users/?" + options?.params.join("&"),
+      options?.limit,
+    );
+    return res.map((ue) => new EventsUsers(<IEventUser> ue));
+  }
 }
